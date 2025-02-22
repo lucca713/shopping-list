42cbe04e9205e7d61b246d738312ece511c8fd6b
lucca <cerasommalucca@gmail.com>
2025-02-22 11:46:59 -0300

    funcao de adicionar produtos parcialmente pronta

--- programacao.js
+++ programacao.js
@@ -1,6 +1,23 @@
 const product = document.querySelector("#input-produtos")
 const form = document.querySelector("form")
 
+const lista = document.querySelector("ul")
+
+let newProductName = document.createElement("li")
+
+let newSpanFrase = document.createElement("span")
+newSpanFrase.setAttribute("id","newSpanFrase")
+
+const newInput = document.createElement("input")
+newInput.setAttribute("type","checkbox")
+
+const newSpanImg = document.createElement("span")
+newSpanImg.classList.add("delete")
+newSpanImg.setAttribute("id","newSpanImg")
+
+const newImg = document.createElement("img")
+newImg.src="img/waste-stroke-rounded.svg"
+
 product.addEventListener("input",(e)=>{
     e.preventDefault()
     console.log(product.value)
@@ -14,17 +31,29 @@ product.addEventListener("input",(e)=>{
 
 })
 
+console.log(lista)
+
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
-    alert(product.value)
+    creamCamp(product.value)
+    console.log(newSpanFrase)
 })
 
 
-function creamCamp(){
+function creamCamp(name){
     try{
 
+        newSpanFrase.textContent= name
+
+        newSpanImg.append(newImg)
+        
+        newProductName.append(newInput,  newSpanFrase, newSpanImg )
+        
+        lista.append(newProductName)
+        
     }catch{
         alert("erro em adicionar o pruduto")
     }
 }
+
 //texxtcontent
