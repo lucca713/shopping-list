2ffef4f65d26e41d179b75fc45765ef9b01f9f17
lucca <cerasommalucca@gmail.com>
2025-02-22 15:18:07 -0300

    colocando o contador de id e a lista funcionando

--- programacao.js
+++ programacao.js
@@ -2,21 +2,9 @@ const product = document.querySelector("#input-produtos")
 const form = document.querySelector("form")
 
 const lista = document.querySelector("ul")
+let listaDosProdutos = []
+let num = 0
 
-let newProductName = document.createElement("li")
-
-let newSpanFrase = document.createElement("span")
-newSpanFrase.setAttribute("id","newSpanFrase")
-
-const newInput = document.createElement("input")
-newInput.setAttribute("type","checkbox")
-
-const newSpanImg = document.createElement("span")
-newSpanImg.classList.add("delete")
-newSpanImg.setAttribute("id","newSpanImg")
-
-const newImg = document.createElement("img")
-newImg.src="img/waste-stroke-rounded.svg"
 
 product.addEventListener("input",(e)=>{
     e.preventDefault()
@@ -24,10 +12,7 @@ product.addEventListener("input",(e)=>{
     const regex = /\D+/g //string
     const regexNumber =  /[^a-zA-Z]/g //regex_number
 
-    product.value != regex ? 
-     product.value = product.value.replace(regexNumber,"") 
-     :
-     null
+    product.value != regex ? product.value = product.value.replace(regexNumber,"") : null
 
 })
 
@@ -36,22 +21,47 @@ console.log(lista)
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
     creamCamp(product.value)
-    console.log(newSpanFrase)
+    listaDosProdutos.push(product.value)
+    
+    
 })
 
 
 function creamCamp(name){
     try{
     if(name){
+        //variaveis criadas na funcao, para ser criadas varias vezes
+         num += 1  //soma de id  
+        let newProductName = document.createElement("li")
+        newProductName.setAttribute("id",`newProductName${num}`)
+        
+
+        let newSpanFrase = document.createElement("span")
+        newSpanFrase.setAttribute("id","newSpanFrase")
+
+        const newInput = document.createElement("input")
+        newInput.setAttribute("type","checkbox")
+
+        const newSpanImg = document.createElement("span")
+        newSpanImg.classList.add("delete")
+        newSpanImg.setAttribute("id","newSpanImg")
+
+        const newImg = document.createElement("img")
+        newImg.src="img/waste-stroke-rounded.svg"
+
+        //fim criacao variaveis
+
         newSpanFrase.textContent= name
 
         newSpanImg.append(newImg)
         
-        newProductName.append(newInput,  newSpanFrase, newSpanImg )
+        newProductName.append(newInput, newSpanFrase, newSpanImg )
         
         lista.appendChild(newProductName)
     }
+
     }catch{
+
         alert("Escreva o produto que você queira adicionar")
     }
 }
